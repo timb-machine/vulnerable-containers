9b0d44f1dba4923fb978c4c2f2ddae52f670b51a
Tim Brown <timb@Tims-MacBook-Air.local>
2023-05-13 21:21:45 +0100

    Updated src/tools/build.sh and src/tools/update-patches.sh to reflect revised structure

--- src/tools/build.sh
+++ src/tools/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd issuebot && docker build -t issuebot:twadhwab . && cd ..
-cd mybackup && docker compose build && cd ..
-cd i2pjump && docker build -t i2pjump:twadhwab . && cd ..
-cd docker-atlassian-stash && docker build -t docker-atlassian-stash:twadhwab . && cd ..
-cd freqtrade && docker build -t freqtrade:twadhwab . && cd ..
-cd redmine/5.0 && docker build -t redmine_5.0:twadhwab . && cd ../..
-cd passbolt_docker && docker build -t passbolt_docker_dev:twadhwab -f dev/Dockerfile . && cd ..
+cd container-artefacts/issuebot && docker build -t issuebot:twadhwab . && cd ../..
+cd container-artefacts/mybackup && docker compose build && cd ../..
+cd container-artefacts/i2pjump && docker build -t i2pjump:twadhwab . && cd ../..
+cd container-artefacts/docker-atlassian-stash && docker build -t docker-atlassian-stash:twadhwab . && cd ../..
+cd container-artefacts/freqtrade && docker build -t freqtrade:twadhwab . && cd ..
+cd container-artefacts/redmine/5.0 && docker build -t redmine_5.0:twadhwab . && cd ../../..
+cd container-artefacts/passbolt_docker && docker build -t passbolt_docker_dev:twadhwab -f dev/Dockerfile . && cd ../..
--- src/tools/update-patches.sh
+++ src/tools/update-patches.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-for filename in docker-atlassian-stash i2pjump mybackup passbolt_docker freqtrade issuebot redmine
+for filename in container-artefacts/docker-atlassian-stash container-artefacts/i2pjump container-artefacts/mybackup container-artefacts/passbolt_docker container-artefacts/freqtrade container-artefacts/issuebot container-artefacts/redmine
 do
 	cd "${filename}"
-	git diff >"../patches/${filename}.patch"
-	cd ..
+	git diff >"../../src/patches/${filename}.patch"
+	cd ../..
 done
